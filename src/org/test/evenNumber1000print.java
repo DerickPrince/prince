540f63a20dfb3ae7c47c78e3cecaac7aba2f79d3
DerickPrince <derickd564@gmail.com>
2022-03-15 20:31:30 +0530

    Sucessfull

--- src/org/test/evenNumber1000print.java
+++ src/org/test/evenNumber1000print.java
@@ -4,6 +4,9 @@ public class evenNumber1000print {
 
 	public static void main(String[] args) throws InterruptedException {
 		
+		System.out.println("==============");
+		
+		
 	for (int i = 0; i < 1000; i++) {
 			
 			if(i%2==0) {
