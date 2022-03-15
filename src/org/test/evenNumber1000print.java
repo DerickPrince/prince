685436a7bdb56ab1f670474fb8cdd5e5191d6b21
DerickPrince <2186264@Cs>
2022-03-15 20:51:52 +0530

    Some Change

--- src/org/test/evenNumber1000print.java
+++ src/org/test/evenNumber1000print.java
@@ -3,6 +3,8 @@ package org.test;
 public class evenNumber1000print {
 
 	public static void main(String[] args) throws InterruptedException {
+	
+		System.out.println("******************");
 		
 	for (int i = 0; i < 1000; i++) {
 			
